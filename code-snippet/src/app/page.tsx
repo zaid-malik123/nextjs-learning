b1f3a829ae3e5b8fa90712f89b8432a4c627eee6
Zaid Malik <mzaidmalik25@gmail.com>
2026-04-02 20:20:42 +0530

    show all snippets

--- code-snippet/src/app/page.tsx
+++ code-snippet/src/app/page.tsx
@@ -1,40 +1,34 @@
-"use client";
-import { useRouter } from "next/navigation";
+import { prisma } from "@/lib/prisma";
+import Link from "next/link";
 
-const Home = () => {
-  const router = useRouter();
+const Home = async () => {
+
+  const snippets = await prisma.snippets.findMany();
+
   return (
     <div className="min-h-screen bg-gray-100 p-6">
 
-      {/* Top Section */}
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-3xl font-bold">Snippets</h1>
 
-        <button onClick={() => router.push("/snippets/new")} className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
+        <Link href="/snippets/new" className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
           New
-        </button>
+        </Link>
       </div>
 
       {/* Snippet List */}
       <div className="space-y-4">
 
-        {/* Card 1 */}
-        <div className="bg-white p-4 rounded-lg shadow flex justify-between items-center">
-          <span>Add two numbers</span>
-          <button onClick={() => router.push("/snippets/1")} className="text-blue-600 hover:underline">View</button>
+        {snippets.map((snippet) => (
+           <div key={snippet.id} className="bg-white p-4 rounded-lg shadow flex justify-between items-center">
+          <span>{snippet.title}</span>
+          <Link href={`/snippets/${snippet.id}`} className="text-blue-600 hover:underline">
+            View
+          </Link>
         </div>
+        ))}
 
-        {/* Card 2 */}
-        <div className="bg-white p-4 rounded-lg shadow flex justify-between items-center">
-          <span>Remove two numbers</span>
-          <button className="text-blue-600 hover:underline">View</button>
-        </div>
-
-        {/* Card 3 */}
-        <div className="bg-white p-4 rounded-lg shadow flex justify-between items-center">
-          <span>Swap two numbers</span>
-          <button className="text-blue-600 hover:underline">View</button>
-        </div>
+        
 
       </div>
 
