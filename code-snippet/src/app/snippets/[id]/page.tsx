81f0633d55708c8675c4d81e522b287bc6b2b0ef
Zaid Malik <mzaidmalik25@gmail.com>
2026-04-02 20:39:49 +0530

    edit snippet

--- code-snippet/src/app/snippets/[id]/page.tsx
+++ code-snippet/src/app/snippets/[id]/page.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import { prisma } from "@/lib/prisma";
+import Link from "next/link";
 
-const SingleSnippet = () => {
+const SingleSnippet = async ({ params }: { params: { id: string } }) => {
+  const id = parseInt((await params).id);
+
+  const snippet = await prisma.snippets.findUnique({
+    where: {
+      id,
+    },
+  });
+
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
       <div className="bg-white p-6 rounded-xl shadow-md w-full max-w-2xl">
         {/* Top Section */}
         <div className="flex justify-between items-center mb-4">
-          <h1 className="text-2xl font-bold">Swap Two Numbers</h1>
+          <h1 className="text-2xl font-bold">{snippet?.title}</h1>
 
           <div className="flex gap-2">
-            <button className="bg-yellow-500 text-white px-4 py-1 rounded-lg hover:bg-yellow-600">
+            <Link
+              href={`/snippets/${id}/edit`}
+              className="bg-yellow-500 text-white px-4 py-1 rounded-lg hover:bg-yellow-600"
+            >
               Edit
-            </button>
+            </Link>
 
             <button className="bg-red-600 text-white px-4 py-1 rounded-lg hover:bg-red-700">
               Delete
@@ -21,11 +33,7 @@ const SingleSnippet = () => {
 
         {/* Code Block */}
         <div className="bg-gray-900 text-green-400 p-4 rounded-lg text-sm overflow-x-auto">
-          {`const swap = (n1, n2) => {
-  const temp = n1;
-  n1 = n2;
-  n2 = temp;
-};`}
+          {`${snippet?.code}`}
         </div>
       </div>
     </div>
